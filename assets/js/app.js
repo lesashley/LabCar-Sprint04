00b519c3719724be0976162ec43c5e3cddaad000
lesahley <lesashley56@gmail.com>
2017-05-29 18:46:56 -0500

    Agregando ubicacion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,36 @@
+var destino = document.getElementById("destino");
+var partida = document.getElementById("partida");
+
 function initMap() {
-       var uluru = {lat: -25.363, lng: 131.044};
-       var map = new google.maps.Map(document.getElementById('mapa'), {
-         zoom: 15,
-         center: uluru
-       });
-       var marker = new google.maps.Marker({
-         position: uluru,
-         map: map
-       });
-     }
+    var iestp = {lat: -12.0520763, lng: -77.0439461};
+    var map = new google.maps.Map(document.getElementById('mapa'), {
+      zoom: 18,
+      center: iestp
+    });
+    var marker = new google.maps.Marker({
+      position: iestp,
+      map: map
+    });
+    var info = new google.maps.InfoWindow({map: map});
 
-
-
-/*
-
-// Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
-
-*/
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          marker:position
+        };
+        info.setPosition(pos);
+        info.setContent('Location found.');
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, info, map.getCenter());
+      });
+    } else {
+      handleLocationError(false, info, map.getCenter());
+    }
+  }
+  function handleLocationError(browserHasGeolocation, info, pos) {
+    info.setPosition(pos);
+    alert("No ha podido ser ubicado");
+  }
